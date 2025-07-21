a629b0bdc2ed8d9230c6a2cb84d79dcc29119f70
AdenilsonNegri <adenilsonegri@gmail.com>
2025-07-20 23:24:13 -0300

    Titulo e subtitulo do Projeto

---
+++ meu-app-react/src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+function App() {
+
+  return (
+    <div>
+
+      <header className="header-app">
+        <h1>Energia Solar 365</h1>
+        <p>Economize com o sol. Invista no seu Futuro!</p>
+      </header>
+
+
+
+    </div>
+  );
+}
+
+export default App;
---
+++ meu-app-react/src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+function App() {
+  return (
+    <header className="header-app">
+        <h1>Energia Solar 365</h1>
+        <p>Economize com o sol. Invista no seu Futuro!</p>
+        <nav>
+            <a href="#home">Home</a>
+            <a href="#about">About</a>
+            <a href="#services">Services</a>
+            <a href="#contact">Contact</a>         
+        </nav>
+    </header>  
+  );
+}
+
+export default App;
